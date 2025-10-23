e9a9fdb5cdb3429865caa292c8264094cc5b5d5f
Sebastian <spiessenberger@gmail.com>
2025-10-23 16:02:43 +0200

    ching chong

--- src/behaviors/line_following/return_to_line.py
+++ src/behaviors/line_following/return_to_line.py
@@ -20,21 +20,23 @@ class ReturnToLine(BTNode):
         self.robot.open_gripper()
         left_color, right_color = self.robot.get_color_sensor_readings()
         print("Return to line - Left color:", left_color, "Right color:", right_color)
-        if left_color >= 20 and right_color < 20:
-            self.robot.set_wheel_duty_cycles(left=0, right=0)
-            print("right black")
-            time.sleep(3)
-            self.robot.set_wheel_duty_cycles(left=-30, right=RETURN_TO_LINE_BASE_SPEED)
-        if right_color >= 20 and left_color < 20:
-            self.robot.set_wheel_duty_cycles(left=0, right=0)
-            print("left black")
-            time.sleep(3)
-            self.robot.set_wheel_duty_cycles(left=RETURN_TO_LINE_BASE_SPEED, right=-30)
         if left_color < 20 and right_color < 20:
             print("on line")
             self.robot.set_wheel_duty_cycles(left=0, right=0)
             time.sleep(5)
             return BTStatus.SUCCESS
+        if left_color >= 20 and right_color < 20:
+            self.robot.set_wheel_duty_cycles(left=0, right=0)
+            print("right black")
+            time.sleep(3)
+            self.robot.set_wheel_duty_cycles(left=-30, right=RETURN_TO_LINE_BASE_SPEED)
+            return BTStatus.RUNNING
+        if right_color >= 20 and left_color < 20:
+            self.robot.set_wheel_duty_cycles(left=0, right=0)
+            print("left black")
+            time.sleep(3)
+            self.robot.set_wheel_duty_cycles(left=RETURN_TO_LINE_BASE_SPEED, right=-30)
+            return BTStatus.RUNNING
         else:
             self.robot.set_wheel_duty_cycles(left=-RETURN_TO_LINE_BASE_SPEED, right=-RETURN_TO_LINE_BASE_SPEED)
             return BTStatus.RUNNING
