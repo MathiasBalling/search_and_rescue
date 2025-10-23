8b6450476ce85cfc66277e621f4f0b15a8e230f6
Sebastian <spiessenberger@gmail.com>
2025-10-23 15:51:00 +0200

    changes

--- src/behaviors/line_following/return_to_line.py
+++ src/behaviors/line_following/return_to_line.py
@@ -12,36 +12,36 @@ class ReturnToLine(BTNode):
         self.robot = robot
         self.blackboard = blackboard
         self.returned_to_line = False
+        self.right_on_line = False
+        self.left_on_line = False
+        self.both_on_line = False
 
     def tick(self) -> BTStatus:
-        if self.returned_to_line:
+        if self.returned_to_line == True:
             return BTStatus.SUCCESS
 
         self.robot.open_gripper()
-        self.robot.set_wheel_duty_cycles(
-            left=-RETURN_TO_LINE_BASE_SPEED, right=-RETURN_TO_LINE_BASE_SPEED
-        )
         left_color, right_color = self.robot.get_color_sensor_readings()
         print("Return to line - Left color:", left_color, "Right color:", right_color)
         if left_color >= 20 and right_color < 20:
             self.robot.set_wheel_duty_cycles(left=0, right=0)
             print("right black")
+            self.right_on_line = True
             time.sleep(3)
             self.robot.set_wheel_duty_cycles(left=-30, right=RETURN_TO_LINE_BASE_SPEED)
-            if left_color < 20 and right_color < 20:
-                print("on line")
-                self.robot.set_wheel_duty_cycles(left=0, right=0)
-                time.sleep(5)
-                return BTStatus.SUCCESS
-        elif right_color >= 20 and left_color < 20:
+        if right_color >= 20 and left_color < 20:
             self.robot.set_wheel_duty_cycles(left=0, right=0)
             print("left black")
+            self.left_on_line = True
             time.sleep(3)
             self.robot.set_wheel_duty_cycles(left=RETURN_TO_LINE_BASE_SPEED, right=-30)
-            if left_color < 20 and right_color < 20:
-                print("on line")
-                self.robot.set_wheel_duty_cycles(left=0, right=0)
-                time.sleep(5)
-                return BTStatus.SUCCESS
-        return BTStatus.RUNNING
+        if left_color < 20 and right_color < 20:
+            print("on line")
+            self.both_on_line = True
+            self.robot.set_wheel_duty_cycles(left=0, right=0)
+            time.sleep(5)
+            return BTStatus.SUCCESS
+        else:
+            self.robot.set_wheel_duty_cycles(left=-RETURN_TO_LINE_BASE_SPEED, right=-RETURN_TO_LINE_BASE_SPEED)
+            return BTStatus.RUNNING
 
