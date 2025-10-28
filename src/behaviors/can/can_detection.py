aecd1fabce762924fd2b5fc4f7570a22fce6f0d4
Mathias Balling <math260r@gmail.com>
2025-10-28 15:42:38 +0100

    Fix logic

--- src/behaviors/can/can_detection.py
+++ src/behaviors/can/can_detection.py
@@ -1,3 +1,4 @@
+import time
 from behavior_tree import BTStatus, BTNode
 from blackboard import BlackBoard
 from robot import EV3Robot
@@ -5,6 +6,7 @@ from robot import EV3Robot
 from params import (
     CAN_DETECTION_BASE_SPEED,
     CAN_DETECTION_DISTANCE_THRESHOLD,
+    TURN_TIME_PER_DEGREE,
 )
 
 
@@ -16,23 +18,27 @@ class CanDetection(BTNode):
         self.turn_counter = 0
         self.turn_duration = 20
         self.turned_right = False
+        self.start_right_turn = None
 
     def tick(self) -> BTStatus:
         if self.can_found:
             return BTStatus.SUCCESS
 
+        if self.start_right_turn is None:
+            self.start_right_turn = time.time()
+
         distance = self.robot.get_ultrasound_sensor_reading()
         if distance <= CAN_DETECTION_DISTANCE_THRESHOLD:
-            self.robot.set_wheel_duty_cycles(
-                left=CAN_DETECTION_BASE_SPEED, right=CAN_DETECTION_BASE_SPEED
-            )
             self.can_found = True
         else:
             # Turn right or left based on current state
             if not self.turned_right:
                 # Turn right
-                self.robot.turn_deg(30)
-                self.turned_right = True
+                self.robot.set_wheel_duty_cycles(
+                    left=CAN_DETECTION_BASE_SPEED, right=-CAN_DETECTION_BASE_SPEED
+                )
+                if time.time() - self.start_right_turn > 1.0:
+                    self.turned_right = True
             else:
                 self.robot.set_wheel_duty_cycles(
                     left=-CAN_DETECTION_BASE_SPEED, right=CAN_DETECTION_BASE_SPEED
