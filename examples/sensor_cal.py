7df03eddc035976b89c2ea71e7e095d83bf78550
Sebastian <spiessenberger@gmail.com>
2025-11-25 12:24:35 +0100

    remove gyro

--- examples/sensor_cal.py
+++ examples/sensor_cal.py
@@ -18,15 +18,12 @@ cl_sensor_left.mode = ev3.ColorSensor.MODE_COL_REFLECT
 cl_sensor_right = ev3.ColorSensor(ev3.INPUT_4)
 cl_sensor_right.mode = ev3.ColorSensor.MODE_COL_REFLECT
 
-gyro = ev3.GyroSensor(ev3.INPUT_3)
-
 assert mL.connected, "Motor A is not connected to port A"
 assert mR.connected, "Motor B is not connected to port D"
 
 assert cl_sensor_left.connected, "Color sensor is not connected to port 1"
 assert cl_sensor_right.connected, "Color sensor is not connected to port 4"
 assert us.connected, "Ultrasonic sensor is not connected to port 2"
-assert gyro.connected, "Gyro sensor is not connected to port 3"
 
 
 mL.run_direct()
